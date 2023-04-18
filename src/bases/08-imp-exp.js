df901ae690393caa03a825c4343deebf38029b86
JuanPabloCostas <jcostas29@alumnos.uaq.mx>
2023-04-18 14:35:48 -0600

    yey

--- src/bases/08-imp-exp.js
+++ src/bases/08-imp-exp.js
@@ -4,17 +4,18 @@ import  heroes,{ owners } from "../data/heroes";
 
 
 
-console.log( owners );
+// console.log( owners );
 
 
 
-const  getHeroeById = (id) => heroes.find( heroe => heroe.id === id );
+export const  getHeroeById = (id) => heroes.find( heroe => heroe.id === id );
 
-console.log( getHeroeById(1) );
+// console.log( getHeroeById(1) );
 
 
 
 // filter
-const getHerosByOwner = (owner) => heroes.filter( heroe => heroe.owner === owner );
+export const getHerosByOwner = (owner) => heroes.filter( heroe => heroe.owner === owner );
 
-console.log( getHerosByOwner('DC') );  
+// console.log( getHerosByOwner('DC') ); 
+
